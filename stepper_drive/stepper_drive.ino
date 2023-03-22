1f575d91514de0631f06d06eb95fa7cdee7c3ade
Sreeram3927 <sreeram292004@gmail.com>
2023-03-22 09:53:08 +0530

    stepper pins

---
+++ stepper_drive/stepper_drive.ino
@@ -0,0 +1,14 @@
+int a1 = 5;
+int a2 = 6;
+int b1 = 7;
+int b2 = 8;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
