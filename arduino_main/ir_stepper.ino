683d047190ea8095d7c8bc02c05c53920e71a0ac
Sreeram3927 <sreeram292004@gmail.com>
2023-04-20 10:27:18 +0530

    setup ir stepper

--- arduino_main/ir_stepper.ino
+++ arduino_main/ir_stepper.ino
@@ -3,3 +3,12 @@ const int ir_in2 = 3;
 const int ir_in3 = 4;
 const int ir_in4 = 5;
 int i_pos = 0;
+
+void irStepperSetup() {
+
+  pinMode(ir_in1, OUTPUT);
+  pinMode(ir_in2, OUTPUT);
+  pinMode(ir_in3, OUTPUT);
+  pinMode(ir_in4, OUTPUT);
+
+}
