5f270596d9e2099172062e2723b898a7e59886fb
Sreeram3927 <sreeram292004@gmail.com>
2023-04-28 17:29:30 +0530

    add scanObject function

--- arduino_main/arduino_main.ino
+++ arduino_main/arduino_main.ino
@@ -14,14 +14,14 @@ void loop() {
   //rotarySpin();
   //irStepDown(50);
   //delay(500);
-  //rotarySpin(200);
+  //rotarySpin(5);
   //stepperRun_FullStep(6, 7, 8, 9);
   //stepperRun_FullStep(2, 3, 4, 5);
   //sendInt(serialRead());
   //delay(1000);
-  irStepUp(10);
+  //irStepUp(10);
   //irStepperStartup();
-  //startup();
+  startScan();
   delay(1000);
   //startup();
 }
--- arduino_main/commands.ino
+++ arduino_main/commands.ino
@@ -1,6 +1,16 @@
 
+int scanData[40];
+
 void scanObject() {
-  //for (int z = )
+  for (int z = 0; z <= 40; z++) {
+    rotarySpin(5);
+    scanData[z] = irRead();
+  }
+  for (int z = 0; z <= 40; z++) {
+    Serial.print(scanData[z]);
+    Serial.print(" ");
+  }
+  Serial.println("");
 }
 
 void startScan() {
