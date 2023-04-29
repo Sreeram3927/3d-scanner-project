345ff3be11333acd43fccf5f5ca8f0e9c917aed6
Sreeram3927 <sreeram292004@gmail.com>
2023-04-29 12:01:27 +0530

    get and print distance in matlab

--- matlab/matlab_main.m
+++ matlab/matlab_main.m
@@ -3,25 +3,25 @@ baudrate = 9600;
 cmd = 'n';
 
 arduino = serialport(port, baudrate);
-% fopen(arduino);
+fopen(arduino);
 
 scanData = zeros(70, 40, 'int16');
 
-fscanf(cmd);
+cmd = input('Enter command: ', 's');
 
 while cmd ~= 'x'
     if cmd == 'y'
         write(arduino, 3, "int16");
         flush(arduino);
         for i = 1:5
-            while arduino.NumBytesAvailable < 80; end
+            %while arduino.NumBytesAvailable < 80; end
             recieved_data = read(arduino, 40, 'int16');
             scanData(i, :) = recieved_data;
         end
         cmd = 'n';
-        fprintf(scanData);
+        disp(scanData);
     end
-    fscanf(cmd);
+    cmd = input('Enter command: ', 's');
 end
 
 fclose(arduino);
