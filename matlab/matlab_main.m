16554ee0ae4b505495002256b273f74b4daf7137
Sreeram3927 <sreeram292004@gmail.com>
2023-04-29 11:04:59 +0530

    recieve data from serial port

--- matlab/matlab_main.m
+++ matlab/matlab_main.m
@@ -2,6 +2,14 @@ port = 'COM3';
 baudrate = 9600;
 
 arduino = serialport(port, baudrate);
-fopen(arduino);
+% fopen(arduino);
 
 scanData = zeros(70, 40, 'int16');
+
+for i = 1:70
+
+    while s.BytesAvailable < 80
+    end
+    recieved_data = read(arduino, 40, 'int16');
+
+end
