9ee8ebaf8a214bbc5bb87de4e051c3824f2235e9
Sreeram3927 <sreeram292004@gmail.com>
2023-05-02 10:37:00 +0530

    get single line of data

--- matlab/matlab_main.m
+++ matlab/matlab_main.m
@@ -3,13 +3,15 @@ baudrate = 9600;
 cmd = 'n';
 
 arduino = serialport(port, baudrate);
+%arduino.Timeout = 10;
 fopen(arduino);
 
-scanData = zeros(70, 41, 'uint16');
-for i = 1:5
-    while arduino.NumBytesAvailable == 80; end
-    recieved_data = fscanf(arduino, '%u', [1, 41]);
-    scanData(i, :) = recieved_data;
-end
+%scanData = zeros(70, 41, 'uint16');
+%for i = 1:5
+ %   while arduino.NumBytesAvailable == 80; end
+    recieved_data = fscanf(arduino);
+  %  scanData(i, :) = recieved_data;
+%end
+disp(recieved_data);
 fclose(arduino);
 delete(arduino);
