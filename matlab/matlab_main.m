2eb56c8cced788a69e401ce7a8b14fc3ee7deed1
Sreeram3927 <sreeram292004@gmail.com>
2023-05-02 13:04:59 +0530

    send and get 70 depth information in matlab

--- matlab/matlab_main.m
+++ matlab/matlab_main.m
@@ -1,14 +1,14 @@
 port = 'COM3';
 baudrate = 9600;
-cmd = 'n';
+%cmd = 'n';
 
 arduino = serialport(port, baudrate);
-%arduino.Timeout = 10;
+arduino.Timeout = 10;
 fopen(arduino);
 
 scanData = zeros(70, 25, 'uint16');
 
-for i = 1:5
+for i = 1:70
  %   while arduino.NumBytesAvailable == 80; end
     recieved_data = fscanf(arduino, '%d');
     scanData(i, :) = recieved_data(1:25);
